776e78eb4ad678fdcdc5a802ed821faaae97de69
eltonnuness <eltonnuness@users.noreply.github.com>
2019-05-10 19:24:07 -0300

    First scene

---
+++ Assets/Scripts/Plataforma.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataforma : MonoBehaviour
+{
+    public float speed;
+    public float limitX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseHorizontalDirection = Input.GetAxis("Mouse X"); // -1 = left; 0 = idle, 1 = right
+        GetComponent<Transform>().position += Vector3.right * mouseHorizontalDirection * speed * Time.deltaTime;
+        float xCurrent = transform.position.x;
+        xCurrent = Mathf.Clamp(xCurrent, -limitX, limitX);
+        transform.position = new Vector3(xCurrent, transform.position.y, transform.position.z);
+    }
+}
